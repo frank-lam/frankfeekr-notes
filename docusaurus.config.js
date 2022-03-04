a5f5ab51fe0e3b1a124e63913796b047f2ed6496
FrankFeekr <1002039945@qq.com>
2022-03-05 01:47:30 +0800

    修改github配置

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -143,7 +143,7 @@ const config = {
           // },
           // { to: '/blog', label: 'Blog', position: 'left' },
           {
-            href: 'https://github.com/facebook/docusaurus',
+            href: 'https://github.com/frank-lam/fullstack-tutorial',
             label: 'GitHub',
             position: 'right',
           },
